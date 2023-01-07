ba06182695b69ef732d5cf5c8634c1fcca830d3f
linneszyx <itssuryanshcooldude346@gmail.com>
2023-01-07 20:38:49 +0530

    mod bt

--- Binary Tree/bt.cpp
+++ Binary Tree/bt.cpp
@@ -1,35 +1,138 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 class Node
 {
-    public:
+public:
     int data;
     Node *left;
     Node *right;
-    Node(int d){
+    Node(int d)
+    {
         data = d;
         left = right = NULL;
     }
 };
-//Preorder build of the tree root left right tree
-Node * buildTree(){
+Node *buildTree()
+{
     int d;
-    cin>>d;
-    if(d==-1) return NULL;
+    cin >> d;
+    if (d == -1)
+        return NULL;
     Node *n = new Node(d);
     n->left = buildTree();
-    n->right= buildTree();
+    n->right = buildTree();
     return n;
 }
-void printPreorder(Node *root){
-    if(root==NULL)
-    return;
-    cout<<root->data<<" ";
+void printPreorder(Node *root)
+{
+    if (root == NULL)
+        return;
+    cout << root->data << " ";
     printPreorder(root->left);
     printPreorder(root->right);
 }
-int main(){
+void printInorder(Node *root)
+{
+    if (root == NULL)
+        return;
+    printInorder(root->left);
+    cout << root->data << " ";
+    printInorder(root->right);
+}
+void printPostorder(Node *root)
+{
+    if (root == NULL)
+        return;
+    printPostorder(root->left);
+    printPostorder(root->right);
+    cout << root->data << " ";
+}
+/*
+IP : 1 2 4  -1 -1 5 7 -1 -1 -1 3 -1 6 -1 -1
+Expected OP
+1
+23
+456
+7
+*/
+void levelOrderPrint(Node *root)
+{
+    queue<Node *> q;
+    q.push(root);
+    q.push(NULL);
+    while (!q.empty())
+    {
+        Node *temp = q.front();
+        if (temp == NULL)
+        {
+            cout << endl;
+            q.pop();
+            if (!q.empty())
+            {
+                q.push(NULL);
+            }
+        }
+        else
+        {
+            q.pop();
+            cout << temp->data << " ";
+            if (temp->left)
+            {
+                q.push(temp->left);
+            }
+            if (temp->right)
+            {
+                q.push(temp->right);
+            }
+        }
+    }
+    return;
+}
+int height(Node *root)
+{
+    if(root==NULL) return 0;
+    int h1 = height(root->left);
+    int h2 = height(root->right);
+    return 1 + max(h1,h2);
+}
+Node *levelOrderBuild()
+{
+    int d;
+    cin >> d;
+    Node *root = new Node(d);
+    queue<Node *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        Node *curr = q.front();
+        q.pop();
+        int c1, c2;
+        cin >> c1 >> c2;
+        if (c1 != -1)
+        {
+            curr->left = new Node(c1);
+            q.push(curr->left);
+        }
+        if (c2 != -1)
+        {
+            curr->right = new Node(c2);
+            q.push(curr->right);
+        }
+    }
+    return root;
+}
+int main()
+{
     Node *root = buildTree();
+    printInorder(root);
+    cout << "\n";
     printPreorder(root);
-return 0;
+    cout << "\n";
+    printPostorder(root);
+    cout << "\n";
+    levelOrderPrint(root);
+    cout << "\n";
+    Node *root1 = levelOrderBuild();
+    levelOrderPrint(root);
+    return 0;
 }
