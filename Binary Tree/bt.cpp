cac22b14257ad4002ac0cf5e8f8ee0fc71f4731e
linneszyx <itssuryanshcooldude346@gmail.com>
2023-01-07 23:31:08 +0530

    added diameter func

--- Binary Tree/bt.cpp
+++ Binary Tree/bt.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
+// IP : 1 2 4  -1 -1 5 7 -1 -1 -1 3 -1 6 -1 -1
 class Node
 {
 public:
@@ -17,8 +18,10 @@ Node *buildTree()
     int d;
     cin >> d;
     if (d == -1)
+    {
         return NULL;
-    Node *n = new Node(d);
+    }
+    Node *n = new Node(d); 
     n->left = buildTree();
     n->right = buildTree();
     return n;
@@ -48,7 +51,7 @@ void printPostorder(Node *root)
     cout << root->data << " ";
 }
 /*
-IP : 1 2 4  -1 -1 5 7 -1 -1 -1 3 -1 6 -1 -1
+
 Expected OP
 1
 23
@@ -95,6 +98,32 @@ int height(Node *root)
     int h2 = height(root->right);
     return 1 + max(h1,h2);
 }
+int diameter(Node *root){
+    if(root==NULL) return 0;
+    int D1 = height(root->left) + height(root->right);
+    int D2 = diameter(root->left);
+    int D3 = diameter(root->right);
+    return max(D1,max(D2,D3));
+}
+class HIGHDIA{
+    public:
+    int h,d;
+};
+HIGHDIA optDia(Node *root){
+    HIGHDIA p;
+    if(root==NULL){
+        p.h = p.d = 0;
+        return p;
+    }
+    HIGHDIA Left = optDia(root->left);
+    HIGHDIA Right = optDia(root->right);
+    p.h = max(Left.h,Right.h) + 1;
+    int D1 = Left.h + Right.h;
+    int D2 = Left.d;
+    int D3 = Right.d;
+    p.d = max(D1,max(D2,D3));
+    return p;
+}
 Node *levelOrderBuild()
 {
     int d;
@@ -124,15 +153,22 @@ Node *levelOrderBuild()
 int main()
 {
     Node *root = buildTree();
+    // levelOrderPrint(root);
+    cout<<"Diameter "<<diameter(root)<<"\n";
+    cout<<"Optimesed Diameter "<<optDia(root).d<<endl;
+    cout<<"Printing InOrder Traversal\n";
     printInorder(root);
+    cout<<"Printing PreOrder Traversal\n";
     cout << "\n";
     printPreorder(root);
     cout << "\n";
+    cout<<"Printing PostOrder Traversal\n";
     printPostorder(root);
     cout << "\n";
+    cout<<"Printing Level Order Traversal\n";
     levelOrderPrint(root);
     cout << "\n";
-    Node *root1 = levelOrderBuild();
-    levelOrderPrint(root);
+    // Node *root1 = levelOrderBuild();
+    // levelOrderPrint(root);
     return 0;
 }
