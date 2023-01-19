ff6ac189faf7ff50c7cced0e490663319ea5912f
linneszyx <itssuryanshcooldude346@gmail.com>
2023-01-20 02:36:24 +0530

    cmnts on vec

--- Vector/2DVector.cpp
+++ Vector/2DVector.cpp
@@ -1,5 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
+/**
+ * It prints the elements of the 2D vector.
+ * 
+ * @return 1 2 3
+ * 4 5 6
+ * 7 8 9 10
+ * 11 12
+ */
 int main(){
 vector<vector <int> > arr = {
     {1,2,3},
